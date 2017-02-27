2d6dc371abd6c2ad001468f0d163e0df561fdf01
Stiffrock <christofer.malmberg@gmail.com>
2017-02-27 14:50:11 +0100

    bara 3 kvar

--- Adv._c++_uppg/Algoritmer/Source.cpp
+++ Adv._c++_uppg/Algoritmer/Source.cpp
@@ -1,13 +1,40 @@
 #include <algorithm>
 #include <time.h>
 #include <stdlib.h>
+#include <iterator>
 #include <vector>
 #include <iostream>
 #include <random>
 #include <numeric>
+#include <forward_list>
 
 
 using namespace std;
+
+
+
+template <class ForwardIterator>
+bool Compare(ForwardIterator cmp1, ForwardIterator cmp2)
+{
+	return *cmp1 > *cmp2;
+}
+
+
+template <class ForwardIterator>
+void ForwardSort(ForwardIterator begin, ForwardIterator end)
+{
+	ForwardIterator itr;
+	ForwardIterator itr2;
+
+	for (itr = begin; itr != end; itr++)
+	{
+		for (itr2 = begin; itr2 != end; itr2++)
+		{
+			ForwardIterator next = itr2++;
+			Compare(itr2, next);
+		}
+	}
+};
 
 
 void VectorSort()
@@ -73,12 +100,57 @@ void ArraySort()
 	}
 }
 
+void removeif()
+{
+	vector<int> v(20);
+	vector<int>::iterator itr;
+
+	srand((unsigned)time(NULL));
+	int r = rand();
+	iota(begin(v), end(v), r);
+	random_shuffle(begin(v), end(v));
+
+	for (itr = begin(v); itr != end(v); itr++)
+	{
+		cout << *itr << endl;
+	}
+
+	v.erase(remove_if(begin(v), end(v), [](int x) {return (x % 2) == 0; }), end(v));
+
+	cout << "\nOdd numbers only" << endl;
+	for (itr = begin(v); itr != end(v); itr++)
+	{
+		cout << *itr << endl;
+	}	
+}
+
+void Bubblesort()
+{
+	
+
+	forward_list<int> list;
+	srand((unsigned)time(NULL));
+	int r = rand();
+	for (int i = 0; i < 10; i++)
+	{
+		list.push_front(r);
+	}
+
+	ForwardSort(begin(list), end(list));
+
+
+	int x = 0;
+	
+}
+
 
 
 int main()
 {
 	//VectorSort();
-	ArraySort();
+	//ArraySort();
+	//removeif();
+	Bubblesort();
 	std::cin.get();
 	return 0;
 }
