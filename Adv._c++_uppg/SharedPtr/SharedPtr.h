b269d4b2d2292a498a36ff068f6de16228ea5c20
Stiffrock <christofer.malmberg@gmail.com>
2017-03-18 13:50:26 +0100

    Almost done with sharedptr"

--- Adv._c++_uppg/SharedPtr/SharedPtr.h
+++ Adv._c++_uppg/SharedPtr/SharedPtr.h
@@ -4,11 +4,17 @@ template <class T>
 class SharedPtr
 {
 	T* ptr;
-	int *count;
+	int* count;
 public:
 	SharedPtr();
 	SharedPtr(std::nullptr_t nullp);
 	SharedPtr(T* ptr);
+
+	void Invariant() 
+	{
+		assert((ptr == nullptr && count == nullptr) || (ptr != nullptr && count != nullptr));
+	};
+	
 
 	//Move
 	SharedPtr(SharedPtr&&);
@@ -25,12 +31,17 @@ public:
 	T& operator*() { return *ptr; };
 	T* operator->() { return ptr; };
 	operator bool() { if (ptr != nullptr) { return true; } else return false; };
-	T& operator=(const SharedPtr ptr);
+	SharedPtr<T>& operator=(const SharedPtr& ptr);
 
 	
-	void reset(T* = nullptr) {};
+	void reset(T* nptr = nullptr) 
+	{
+		count = new int(1);
+		ptr = nptr;
+
+	};
 	T* get() { return ptr; };
-	bool unique() { if (*count > 1) { return true; } else return false; }; //checks if the managed oject is owned by others
+	bool unique() { if (*count <= 1) { return true; } else return false; }; //checks if the managed oject is owned by others
 	
 	~SharedPtr();
 };
--- Adv._c++_uppg/SharedPtr/SharedPtr.hpp
+++ Adv._c++_uppg/SharedPtr/SharedPtr.hpp
@@ -1,22 +1,34 @@
 template <typename T>
 SharedPtr<T>::SharedPtr()
 {
+	//count = new int(1);
+
+	count = nullptr;
 	ptr = nullptr;
-	count = new int(1);
+	Invariant();
 }
 
 template <typename T>
 SharedPtr<T>::SharedPtr(std::nullptr_t nullp)
 {
+	//count = new int(1);
+	count = nullptr;
 	ptr = nullptr;
-	count = 0;
+	Invariant();
+
 }
 
 template<typename T>
 SharedPtr<T>::SharedPtr(T* p)
 {
-	ptr = ptr;
-	count = new int(1);
+	if (p != nullptr)
+		count = new int(1);
+	else
+		count = nullptr;
+
+	ptr = p;
+	Invariant();
+
 }
 
 template<typename T>
@@ -26,7 +38,9 @@ SharedPtr<T>::SharedPtr(SharedPtr<T> &&rhs)
 	count = rhs.count;
 
 	rhs.ptr = nullptr;
-	*rhs.count = 0;
+	rhs.count = nullptr;
+	Invariant();
+
 }
 
 template <typename T>
@@ -34,39 +48,84 @@ SharedPtr<T>::SharedPtr(const SharedPtr<T> &rhs)
 {
 	ptr = rhs.ptr;
 	count = rhs.count;
+
+	if (ptr != nullptr)
 	++*count;
+
+	Invariant();
+
 }
-
-
-
 
 template <typename T>
 SharedPtr<T>::~SharedPtr()
 {
+	if (count == nullptr)
+	{
+		return;
+	}
 	if (--*count == 0)
 	{
 		delete ptr;
 		delete count;
 	}
+
 }
 
 template <typename T>
 SharedPtr<T> &SharedPtr<T>::operator=(SharedPtr<T> &&rhs)
-{
-	++*rhs.count;
-	ptr = rhs.ptr;
-	count = rhs.count;
+{	
+
+	if (ptr == sptr.ptr)
+		return *this;
+
+	if (ptr != nullptr)
+	{
+		ptr = sptr.ptr;
+		count = sptr.count;
+	}
+	if (sptr.ptr == nullptr)
+	{
+		ptr = sptr.ptr;
+		count = nullptr;
+	}
+	//ptr = rhs.ptr;
+	//count = rhs.count;
+
+	if (ptr != nullptr)
+		++*rhs.count;
+	Invariant();
+
 	return *this;
 }
 
 template <typename T>
-T& SharedPtr<T>::operator=(const SharedPtr sptr)
+SharedPtr<T>& SharedPtr<T>::operator=(const SharedPtr& sptr) //Tilldelningsoperatorerna är fucked, måste ta hänsyn till att du kan göra tilldelning på ett redan existerande objekt. Då måste det objektet tas bort LUL.
 {
-	this->ptr = sptr.ptr;
-	++*sptr.count;
-	this->count = sptr.count;
+
+	if (ptr == sptr.ptr)
+		return *this;
+
+	if (ptr != nullptr)
+	{
+		ptr = sptr.ptr;
+		count = sptr.count;
+	}
+	if (sptr.ptr == nullptr)
+	{
+		ptr = sptr.ptr;
+		count = nullptr;
+	}
+		
+
+
+	
+	
+
+	if (ptr != nullptr)
+		++*sptr.count;
+	Invariant();
 	return *this;
-}s
+}
 
 
 
